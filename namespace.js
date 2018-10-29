f7846e123f511b42c0ce950968711b5b7b27ac4a
dvmufuta <43271009+dvmufuta@users.noreply.github.com>
2018-10-29 11:23:48 -0400

    Add files via upload

---
+++ namespace.js
@@ -0,0 +1,33 @@
+/*******************************
+Filename: namespace.js
+Author: @Dv Mufuta
+Description: Namespace Assignment
+Date: 2018-10-26
+********************************/
+
+
+var MUFU0004 = {
+    init: function () {
+
+        let div = document.createElement("div");
+        div.className = "box";
+        div.textContent = "MUFU0004";
+        
+        document.querySelector("#boxes").appendChild(div);
+
+
+        div.addEventListener("click", function () {
+            div.style.borderColor = "#008000";
+            div.style.backgroundColor = "#252A98";
+        });
+        div.addEventListener("mouseover", function () {
+            div.classList.toggle("highlight");
+        });
+        div.addEventListener("mouseout", function () {
+            div.classList.toggle("highlight");
+            div.removeAttribute("style");
+        });
+
+    }
+};
+
